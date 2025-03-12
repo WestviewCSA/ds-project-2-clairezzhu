21ae3661f7d97727fcf16f139866be5ca74171a5
1949381 <1949381@01-18123.pusd.dom>
2025-03-12 13:46:30 -0700

    currently working on code

--- P2_first_last/src/p2.java
+++ P2_first_last/src/p2.java
@@ -20,20 +20,40 @@ public class p2 {
 			int numCols = scanner.nextInt();
 			int numRooms = scanner.nextInt();
 			int rowIndex = 0;
-			
-			//process the map!
-			while(scanner.hasNextLine()) {
-				//grab a line (one row of the map)
-				String row = scanner.nextLine();
-				System.out.println(row);
-				
-				if(row.length() > 0) {
-					for(int i = 0; i<numCols && i<row.length(); i++) {
-						char el = row.charAt(i);
-						Tile obj = new Tile(rowIndex, i, el);
-					}
-				}
-			}
+			scanner.nextLine(); // Consume the leftover newline after reading integers
+
+            // Read the maze
+            char[][] maze = new char[numRows][numCols];
+            int startRow = -1, startCol = -1;
+            int endRow = -1, endCol = -1;
+
+            for (int i = 0; i < numRows; i++) {
+                String row = scanner.nextLine().trim();
+                for (int j = 0; j < numCols; j++) {
+                    maze[i][j] = row.charAt(j);
+                    if (maze[i][j] == 'W') {
+                        startRow = i;
+                        startCol = j;
+                    } else if (maze[i][j] == '$') {
+                        endRow = i;
+                        endCol = j;
+                    }
+                }
+            }
+//            
+//			//process the map!
+//			while(scanner.hasNextLine()) {
+//				//grab a line (one row of the map)
+//				String row = scanner.nextLine();
+//				System.out.println(row);
+//				
+//				if(row.length() > 0) {
+//					for(int i = 0; i<numCols && i<row.length(); i++) {
+//						char el = row.charAt(i);
+//						Tile obj = new Tile(rowIndex, i, el);
+//					}
+//				}
+//			}
 		}catch (FileNotFoundException e) {
 			System.out.println(e);
 		}
