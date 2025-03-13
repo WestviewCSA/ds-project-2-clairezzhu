9b980a2f34052d1c46d8d67c0fbeaa340c24f5f8
1949381 <1949381@01-18123.pusd.dom>
2025-03-13 13:28:05 -0700

    find (x,y) coordinates of "W" and"$"

--- P2_first_last/src/p2.java
+++ P2_first_last/src/p2.java
@@ -16,44 +16,49 @@ public class p2 {
 			File file = new File(filename);
 			Scanner scanner = new Scanner(file);
 			
+			//figure out how many rows, cols, and rooms there are for the map
 			int numRows = scanner.nextInt();
 			int numCols = scanner.nextInt();
 			int numRooms = scanner.nextInt();
 			int rowIndex = 0;
 			scanner.nextLine(); // Consume the leftover newline after reading integers
+			
+			Tile[][][] tiles = new Tile[numRows][numCols][numRooms];
+			
+			int wX = -1;
+			int wY = -1;
+			
+			int $X = -1;
+			int $Y = -1;
 
-            // Read the maze
-            char[][] maze = new char[numRows][numCols];
-            int startRow = -1, startCol = -1;
-            int endRow = -1, endCol = -1;
-
-            for (int i = 0; i < numRows; i++) {
-                String row = scanner.nextLine().trim();
-                for (int j = 0; j < numCols; j++) {
-                    maze[i][j] = row.charAt(j);
-                    if (maze[i][j] == 'W') {
-                        startRow = i;
-                        startCol = j;
-                    } else if (maze[i][j] == '$') {
-                        endRow = i;
-                        endCol = j;
-                    }
-                }
-            }
-//            
-//			//process the map!
-//			while(scanner.hasNextLine()) {
-//				//grab a line (one row of the map)
-//				String row = scanner.nextLine();
-//				System.out.println(row);
-//				
-//				if(row.length() > 0) {
-//					for(int i = 0; i<numCols && i<row.length(); i++) {
-//						char el = row.charAt(i);
-//						Tile obj = new Tile(rowIndex, i, el);
-//					}
-//				}
-//			}
+			//process the map!
+			while(scanner.hasNextLine()) {
+				//grab a line (one row of the map)
+				String row = scanner.nextLine();
+				System.out.println(row);
+				
+				//there is data in the row
+				if(row.length() > 0) {
+					//search for each element in the row and make sure it doesn't go out of bounds
+					for(int i = 0; i<numCols && i<row.length(); i++) {
+						char el = row.charAt(i);
+						Tile obj = new Tile(rowIndex, i, el);
+						
+						//find the (x,y) coordinates of the "W"
+						if(obj.getType() == 'W') {
+							//System.out.println("(" + i + ", " + rowIndex + ")");
+							wX = i;
+							wY = rowIndex;
+						}
+						
+						if(obj.getType() == '$') {
+							$X = i;
+							$Y = rowIndex;
+						}
+					}
+					rowIndex++;
+				}
+			}
 		}catch (FileNotFoundException e) {
 			System.out.println(e);
 		}
