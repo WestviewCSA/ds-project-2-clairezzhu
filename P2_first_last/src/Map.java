c585f23b7a02930dd3e1939a9259ca060af9b8b6
1949381 <1949381@01-18123.pusd.dom>
2025-03-14 13:31:30 -0700

    updated Map class

--- P2_first_last/src/Map.java
+++ P2_first_last/src/Map.java
@@ -1,4 +1,31 @@
 
 public class Map {
 	private Tile[][] map;
+	
+	char[][] maze;
+	int rows, cols;
+	Tile start, goal;
+	
+	//initalize the values for the maps
+	public Map(int rows, int cols) {
+		this.rows = rows;
+		this.cols = cols;
+		this.maze = new char[rows][cols];
+	 }
+	
+	public void setTile(int row, int col, char val) {
+		maze[row][col] = val;
+		
+		if(val == 'W') {
+			start = new Tile(row, col, 'W');
+		}
+		
+		if(val == '$') {
+			goal = new Tile(row, col, '$');
+		}
+	}
+	
+	public char getTile(int row, int col) {
+		return maze[row][col];
+	}
 }
